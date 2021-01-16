50e6b02d9f567d827aa8d3629f57440490d44c85
Errol Cleary <errol.cleary@gmail.com>
2021-01-15 17:17:16 -0800

    Create database schema and model

---
+++ database/Listing.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const listingSchema = new Schema({
+  nightlyRate: { type: Number, required: true },
+  averageRating: { type: Number, required: true, default: 0 },
+  reviewCount: { type: Number, required: true, default: 0 },
+  minNights: { type: Number, required: true, default: 1 },
+  bookedDates: [
+    {
+      start: { type: Date },
+      length: { type: Number },
+    },
+  ],
+  maxGuests: { type: Number, required: true, default: 2 },
+  cleaningFee: { type: Number, required: true, default: 0 },
+  serviceFee: { type: Number, required: true, default: 0 },
+  occupancyTaxesAndFees: { type: Number, required: true, default: 0 },
+  discountWeekly10: { type: Number, required: true, default: 0 },
+  discountWeekly20: { type: Number, required: true, default: 0 },
+  discountMonthly20: { type: Number, required: true, default: 0 },
+},
+{
+  timestamps: true,
+});
+
+module.exports.Listing = mongoose.model('Listing', listingSchema);
--- database/index.js
+++ database/index.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const {Schema} = mongoose;
-
 const host = 'localhost';
 const port = '27017';
 const database = 'zenrent';
@@ -19,12 +17,13 @@ mongoose.connect(uri, options, (err) => {
     console.error(err);
   } else {
     connection = mongoose.connection;
-    console.log(`Connected to MongoDB database ${connection._connectionString}`);
+    const { _connectionString } = connection;
+    console.log(`Connected to MongoDB database ${_connectionString}`);
     connection.on('error', (err) => {
       console.error(err);
     });
     process.on('SIGINT', () => {
-      const {pid} = process;
+      const { pid } = process;
       connection.close();
       console.log('\nMongoDB connection closed');
       console.log(`About to exit Node process ID ${pid}`);
