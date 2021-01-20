d8ed06f3dd7b9c65de0539c7162c7963b8899c05
Errol Cleary <errol.cleary@gmail.com>
2021-01-19 22:07:53 -0800

    Refactor date input boxes as separate child component

---
+++ client/src/components/DateInput.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const { Component } = React;
+
+export default class DateInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+  }
+
+  handleInput(event) {
+    const { value } = event.target;
+    this.setState({
+      input: value,
+    });
+  }
+
+  clearInput(event) {
+    event.preventDefault();
+    this.setState({
+      input: '',
+    });
+  }
+
+  render() {
+    const { input } = this.state;
+    const {
+      dateType,
+      onFieldFocus,
+      onFieldBlur,
+      formFocus,
+      fieldFocus,
+    } = this.props;
+    const dateTypeWithoutHyphen = dateType.indexOf('-') > -1
+      ? dateType.slice(0, dateType.indexOf('-')) + dateType.slice(dateType.indexOf('-') + 1)
+      : dateType;
+    const formAndFieldFocusValue = dateTypeWithoutHyphen.slice(0, 5)
+      + dateTypeWithoutHyphen.charAt(5).toUpperCase()
+      + dateTypeWithoutHyphen.slice(6);
+    const formAndFieldName = `${formAndFieldFocusValue}Input`;
+    const fieldId = `${formAndFieldName}Field`;
+    const labelText = dateType.charAt(0).toUpperCase() + dateType.slice(1);
+
+    return (
+      <td style={formFocus === formAndFieldFocusValue
+        ? { border: '3px black solid' }
+        : { border: '1px black solid' }}
+      >
+        <form
+          name={formAndFieldName}
+          onReset={this.clearInput}
+          onFocus={onFieldFocus}
+          onBlur={onFieldBlur}
+        >
+          <label htmlFor={fieldId}>
+            <div style={{ pointerEvents: 'none', textTransform: 'uppercase' }}>
+              {labelText}
+            </div>
+            <input
+              type="text"
+              name={formAndFieldName}
+              id={fieldId}
+              value={input}
+              onChange={this.handleInput}
+              placeholder={fieldFocus === formAndFieldFocusValue
+                ? 'MM/DD/YYYY'
+                : 'Add date'}
+            />
+          </label>
+          {input !== '' ? <input type="reset" value="×" /> : null}
+        </form>
+      </td>
+    );
+  }
+}
+
+DateInput.propTypes = {
+  dateType: PropTypes.string.isRequired,
+  onFieldFocus: PropTypes.func.isRequired,
+  onFieldBlur: PropTypes.func.isRequired,
+  formFocus: PropTypes.string.isRequired,
+  fieldFocus: PropTypes.string.isRequired,
+};
--- client/src/components/Dates.jsx
+++ client/src/components/Dates.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DateInput from './DateInput';
 import Calendar from './Calendar';
 
 const { Component } = React;
@@ -7,27 +8,15 @@ export default class Dates extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // DatesFocused: false, // Requires click (not blur) handling to implement
       DatesMaximized: false,
       formFocus: '',
       fieldFocus: '',
-      checkInInput: '',
-      checkOutInput: '',
     };
-    // this.handleDatesClick = this.handleDatesClick.bind(this);
     this.handleDatesFocus = this.handleDatesFocus.bind(this);
     this.handleDatesBlur = this.handleDatesBlur.bind(this);
     this.handleFieldFocus = this.handleFieldFocus.bind(this);
     this.handleFieldBlur = this.handleFieldBlur.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.clearInput = this.clearInput.bind(this);
   }
-
-  // handleDatesClick() {
-  //   this.setState({
-  //     DatesFocused: true,
-  //   });
-  // }
 
   handleDatesFocus() {
     this.setState({
@@ -36,13 +25,6 @@ export default class Dates extends Component {
   }
 
   handleDatesBlur(event) {
-    // console.log('event.target:');
-    // console.log(event.target);
-    // console.log('event.currentTarget:');
-    // console.log(event.currentTarget);
-    // console.log('event.relatedTarget:');
-    // console.log(event.relatedTarget);
-    // if (event.target === event.currentTarget) {
     if (event.relatedTarget === null) {
       this.setState({
         DatesMaximized: false,
@@ -67,76 +49,16 @@ export default class Dates extends Component {
     }
   }
 
-  handleInput(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  clearInput(event) {
-    event.preventDefault();
-    const { name } = event.target;
-    this.setState({
-      [name]: '',
-    });
-  }
-
   render() {
     const {
-      // DatesFocused,
       DatesMaximized,
       formFocus,
-      checkInInput,
-      checkOutInput,
       fieldFocus,
     } = this.state;
-    const dateInputBox = (dateType) => {
-      const dateTypeWithoutHyphen = dateType.indexOf('-') > -1
-        ? dateType.slice(0, dateType.indexOf('-')) + dateType.slice(dateType.indexOf('-') + 1)
-        : dateType;
-      const formAndFieldFocusValue = dateTypeWithoutHyphen.slice(0,5)
-        + dateTypeWithoutHyphen.charAt(5).toUpperCase()
-        + dateTypeWithoutHyphen.slice(6);
-      const formAndFieldName = `${formAndFieldFocusValue}Input`;
-      const fieldId = `${formAndFieldName}Field`;
-      const labelText = dateType.charAt(0).toUpperCase() + dateType.slice(1);
-      return (
-        <td style={formFocus === formAndFieldFocusValue
-          ? { border: '3px black solid' }
-          : { border: '1px black solid' }}
-        >
-          <form
-            name={formAndFieldName}
-            onReset={this.clearInput}
-            onFocus={this.handleFieldFocus}
-            onBlur={this.handleFieldBlur}
-          >
-            <label htmlFor={fieldId}>
-              <div style={{ pointerEvents: 'none', textTransform: 'uppercase' }}>
-                {labelText}
-              </div>
-              <input
-                type="text"
-                name={formAndFieldName}
-                id={fieldId}
-                value={formAndFieldFocusValue === 'checkIn' ? checkInInput : checkOutInput}
-                onChange={this.handleInput}
-                placeholder={fieldFocus === formAndFieldFocusValue
-                  ? 'MM/DD/YYYY'
-                  : 'Add date'}
-              />
-            </label>
-            {checkOutInput !== '' ? <input type="reset" value="×" /> : null}
-          </form>
-        </td>
-      );
-    };
 
     const calendar = DatesMaximized ? <Calendar /> : null;
 
     return (
-      // <div>
       <div
         tabIndex={1}
         onFocus={this.handleDatesFocus}
@@ -146,16 +68,25 @@ export default class Dates extends Component {
         <table>
           <tbody>
             <tr>
-              {/* {dateInputBox('checkIn', 'checkInInput', 'checkInInputField', 'Check-in')}
-              {dateInputBox('checkOut', 'checkOutInput', 'checkOutInputField', 'Checkout')} */}
-              {dateInputBox('check-in')}
-              {dateInputBox('checkout')}
+              <DateInput
+                onFieldFocus={this.handleFieldFocus}
+                onFieldBlur={this.handleFieldBlur}
+                dateType="check-in"
+                formFocus={formFocus}
+                fieldFocus={fieldFocus}
+              />
+              <DateInput
+                onFieldFocus={this.handleFieldFocus}
+                onFieldBlur={this.handleFieldBlur}
+                dateType="checkout"
+                formFocus={formFocus}
+                fieldFocus={fieldFocus}
+              />
             </tr>
           </tbody>
         </table>
         {calendar}
       </div>
-      // </div>
     );
   }
 }
