e63db3fe2ebcfa4e176b34275d5598673544e3b5
Errol Cleary <errol.cleary@gmail.com>
2021-01-28 20:53:20 -0800

    Adapt styling for proxy server

--- client/src/components/Reservation.jsx
+++ client/src/components/Reservation.jsx
@@ -249,8 +249,29 @@ export default class Reservation extends Component {
         </button>
       );
 
+    // const debounce = (fn) => {
+    //   let frame;
+    //   return (...params) => {
+    //     if (frame) {
+    //       cancelAnimationFrame(frame);
+    //     }
+    //     frame = requestAnimationFrame(() => {
+    //       fn(...params);
+    //     });
+    //   };
+    // };
+    // let storedScroll;
+    // const storeScroll = () => {
+    //   storedScroll = window.scrollY;
+    //   document.documentElement.dataset.scroll = window.scrollY;
+    // };
+    // document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+    // storeScroll();
+
     return (
-      <div className={styles.ReservationContainer}>
+      <div
+        className={styles.ReservationContainer}
+      >
         <Header
           nightlyRate={nightlyRate}
           averageRating={averageRating}
